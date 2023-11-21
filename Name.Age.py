f5dd24e04e3ef7320e95a6d6554ca47fe176b53a
guerrerohazael <71238408+guerrerohazael@users.noreply.github.com>
2023-11-21 09:45:53 -0600

    Add files via upload

---
+++ Name.Age.py
@@ -0,0 +1,13 @@
+# Prompt the user for their name
+name = input("What is your name? ")
+
+# Prompt the user for their age
+age = int(input("How old are you? "))
+
+# Calculate the birth year
+current_year = 2023
+birth_year = current_year - age
+
+
+# Display the result
+print("Hello", name + "! You were born in", birth_year,)
